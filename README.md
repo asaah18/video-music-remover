dda01a14eb588de7d6798ac4dc4584ce1ed31ba5
abdurrahman <32853027+asaah18@users.noreply.github.com>
2024-01-30 21:00:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # video-music-remover
-a Python script to remove music from mp4 video files
+a Python script to remove music from mp4 video files using [demux](https://github.com/facebookresearch/demucs)https://github.com/facebookresearch/demucs machine learning model
+
+## setup
+
+- install [ffmpeg](https://ffmpeg.org) in your device
+- install dependncies using pipenv
+
+## usage
+
+- copy the videos you want to remove the music from to the folder "input"
+- run the Python module `main.py`
+
+the videos without music will be saved to the folder `output` and the original files in `input` folder will be deleted.
---
+++ main.py
@@ -0,0 +1,108 @@
+import logging
+import subprocess
+from pathlib import Path
+
+logging.basicConfig(filename='removing_music.log', encoding='utf-8', format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logging.info('Mass processing started')
+print('Mass processing started')
+
+
+def process_file(original_video: Path):
+    """
+    removes music from videos in input folder
+    :return: True if there's no file to process, which marks the end of the mass process
+    """
+    try:
+        logging.info(f'Processing file "{original_video.name}"')
+        print(f'Processing file "{original_video.name}"')
+        no_music_sound: Path = Path(f'separated/htdemucs/{original_video.stem}/vocals.mp3')
+        if no_music_sound.exists():
+            logging.info(f'"{original_video.name}": vocal already separated, skipping separating vocal')
+            print(f'"{original_video.name}": vocal already separated, skipping separating vocal')
+        else:
+            logging.info(f'"{original_video.name}": start separating vocal...')
+            print(f'"{original_video.name}": start separating vocal...')
+            # TODO: export vocal sound as `wav` extension
+            remove_music_command: list[str] = ['pipenv', 'run', 'demucs', '--mp3', '--two-stems=vocals',
+                                               original_video.absolute()]
+            completed_process = subprocess.run(remove_music_command, encoding='utf-8', text=True,
+                                               capture_output=True,
+                                               check=True)
+            # raise exception if vocal sound is not created, exception raised manually
+            # because demucs command doesn't return error code
+            if not no_music_sound.exists():
+                raise subprocess.CalledProcessError(returncode=1,
+                                                    cmd=completed_process.args,
+                                                    output=completed_process.stdout,
+                                                    stderr=completed_process.stderr)
+            logging.info(f'"{original_video.name}": vocal seperated successfully')
+            print(f'"{original_video.name}": vocal seperated successfully')
+        # 3- replace the sound of the video with the no music version,
+        #  and save the new video in folder 'output'
+        #  if not already done
+        no_music_video: Path = Path(f'output/{original_video.name}')
+        if no_music_video.exists():
+            logging.info(
+                f'"{original_video.name}": a video with no music already exists, skipping creating a new video')
+            print(
+                f'"{original_video.name}": a video with no music already exists, skipping creating a new video')
+        else:
+            logging.info(f'"{original_video.name}": creating a new video with no music...')
+            print(f'"{original_video.name}": creating a new video with no music...')
+            create_no_music_video_command: list[str] = ['ffmpeg', '-i', original_video.absolute(),
+                                                        '-i', no_music_sound.absolute(),
+                                                        '-c:v', 'copy', '-map', '0:v:0', '-map', '1:a:0',
+                                                        no_music_video.absolute()]
+            subprocess.run(create_no_music_video_command, encoding='utf-8', text=True, capture_output=True, check=True)
+            logging.info(f'"{original_video.name}": a new video with no music has been created')
+            print(f'"{original_video.name}": a new video with no music has been created')
+
+        # clean up
+        logging.info(f'"{original_video.name}": deleting original video...')
+        print(f'"{original_video.name}": deleting original video...')
+        original_video.unlink()
+        logging.info(f'"{original_video.name}": original video deleted successfully')
+        print(f'"{original_video.name}": original video deleted successfully')
+        logging.info(f'"{original_video.name}": deleting vocal sound...')
+        print(f'"{original_video.name}": deleting vocal sound...')
+        for file in [file for file in no_music_sound.parent.iterdir()]:
+            file.unlink()
+        no_music_sound.parent.rmdir()
+        logging.info(f'"{original_video.name}": vocal sound deleted successfully')
+        print(f'"{original_video.name}": vocal sound deleted successfully')
+    except subprocess.CalledProcessError as error:
+        logging.exception(error.stderr)
+        print(error.stderr)
+        raise error
+    except KeyboardInterrupt as error:
+        logging.exception('the process was stopped by user')
+        print('the process was stopped by user')
+        raise error
+    except BaseException as error:
+        logging.exception('Unexpected error happened')
+        print('Unexpected error happened')
+        raise error
+    else:
+        logging.info(f'"{original_video.name}": Processing finished')
+        print(f'"{original_video.name}": Processing finished')
+
+
+def get_original_video() -> Path | None:
+    logging.info('Looking for file to process in folder "input"...')
+    print('Looking for file to process in folder "input"...')
+    input_path: Path = Path('input')
+    return next(input_path.glob("*.mp4"), None)
+
+
+original_video = get_original_video()
+
+while original_video:
+    process_file(original_video)
+    original_video = get_original_video()
+else:
+    logging.info("There's no file to process")
+    print("There's no file to process")
+
+logging.info('Mass processing finished')
+print('Mass processing finished')

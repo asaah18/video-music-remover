a02da65bd0b2cd4682cd1c528583587772702797
abdurrahman <32853027+asaah18@users.noreply.github.com>
2024-12-27 17:14:05 +0300

    Chore/refactor code to make it more composable
    - Extract music separation process using AI model as a separate class with abstract class to enable adding support for other AI models in the future.
    - Create a class that receives a video with music and process it until it becomes a video with no music.
        - this class is aware of the API of the music separator abstract class.
    
    Signed-off-by: abdurrahman <32853027+asaah18@users.noreply.github.com>

---
+++ music_remover.py
@@ -0,0 +1,42 @@
+import subprocess
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+
+class MusicRemover(ABC):
+    def __init__(self, original_video: Path):
+        self._original_video: Path = original_video
+        self.__no_music_sound: Path = self._get_no_music_audio_path(original_video)
+
+    @property
+    def no_music_sound(self) -> Path:
+        return self.__no_music_sound
+
+    @abstractmethod
+    def _get_no_music_audio_path(self, video_path: Path) -> Path:
+        pass
+
+    @abstractmethod
+    def remove_music(self) -> None:
+        """
+        separate vocal from music using machine learning model
+
+        :exception subprocess.CalledProcessError
+        """
+
+    pass
+
+
+class Demucs(MusicRemover):
+    def _get_no_music_audio_path(self, video_path: Path) -> Path:
+        return Path(f'separated/htdemucs/{video_path.stem}/vocals.mp3')
+
+    def remove_music(self) -> None:
+        # TODO: export vocal sound as `wav` extension
+        remove_music_command: list[str] = ['pipenv', 'run', 'demucs', '--mp3', '--two-stems=vocals',
+                                           self._original_video.absolute()]
+        subprocess.run(remove_music_command, encoding='utf-8', check=True)
+        # raise exception if vocal sound is not created
+        # exception raised manually because demucs command doesn't return error code
+        if not self.no_music_sound.exists():
+            raise subprocess.CalledProcessError(returncode=1, cmd=remove_music_command)

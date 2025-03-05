d77e4b6d3ed1cc0950577575ac8d47a85115fd2c
abdurrahman <32853027+asaah18@users.noreply.github.com>
2025-03-05 03:20:19 +0300

    Feature/store temporary intermediate files in a temporary directory
    Signed-off-by: abdurrahman <32853027+asaah18@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 from itertools import chain
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Type
 
 import typer
@@ -16,32 +17,32 @@ class RemoveMusicFromVideo:
     def __init__(self, original_video: Path, music_remover_class: Type[MusicRemover], base_directory: Path = None):
         """:raise ValueError if base directory is not a relative path of original video or not an absolute path"""
         self.__original_video = original_video
-        self.__music_remover = music_remover_class(original_video)
+        self.__music_remover_class = music_remover_class
         self.__no_music_video = Path(
             f'output/{original_video.relative_to(base_directory) if base_directory else original_video.name}'
         )
 
     def process(self) -> None:
-        if self.__music_remover.no_music_sound.exists():
-            logging.info(f'"{self.__original_video.name}": vocal already separated, skipping separating vocal')
-        else:
+        logging.info(f'Processing file "{self.__original_video.name}"')
+
+        with TemporaryDirectory(prefix='music-remover-') as temporary_directory:
+            music_remover = self.__music_remover_class(self.__original_video, Path(temporary_directory))
+
             logging.info(f'"{self.__original_video.name}": start separating vocal...')
-            self.__music_remover.remove_music()
+            music_remover.remove_music()
             logging.info(f'"{self.__original_video.name}": vocal seperated successfully')
 
-        logging.info(f'"{self.__original_video.name}": creating a new video with no music...')
-        self.__create_video_without_music()
-        logging.info(f'"{self.__original_video.name}": a new video with no music has been created')
+            logging.info(f'"{self.__original_video.name}": creating a new video with no music...')
+            self.__create_video_without_music(music_remover.no_music_sound)
+            logging.info(f'"{self.__original_video.name}": a new video with no music has been created')
 
-        logging.info(f'"{self.__original_video.name}": deleting original video...')
-        self.__cleanup_original_video()
-        logging.info(f'"{self.__original_video.name}": original video deleted successfully')
+            logging.info(f'"{self.__original_video.name}": deleting original video...')
+            self.__cleanup_original_video()
+            logging.info(f'"{self.__original_video.name}": original video deleted successfully')
 
-        logging.info(f'"{self.__original_video.name}": deleting vocal sound...')
-        self.__cleanup_intermediate_audio()
-        logging.info(f'"{self.__original_video.name}": vocal sound deleted successfully')
+        logging.info(f'"{self.__original_video.name}": Processing finished')
 
-    def __create_video_without_music(self) -> None:
+    def __create_video_without_music(self, no_music_sound: Path) -> None:
         """
         replace the sound of the video with the no music version,
         and save the new video in folder 'output'
@@ -56,18 +57,13 @@ class RemoveMusicFromVideo:
 
         # create video without music
         create_no_music_video_command: list[str] = ['ffmpeg', '-y', '-i', self.__original_video.absolute(),
-                                                    '-i', self.__music_remover.no_music_sound.absolute(),
+                                                    '-i', no_music_sound.absolute(),
                                                     '-c:v', 'copy', '-map', '0:v:0', '-map', '1:a:0',
                                                     self.__no_music_video.absolute()]
         subprocess.run(create_no_music_video_command, encoding='utf-8', check=True)
 
     def __cleanup_original_video(self) -> None:
         self.__original_video.unlink()
-
-    def __cleanup_intermediate_audio(self) -> None:
-        for file in [file for file in self.__music_remover.no_music_sound.parent.iterdir()]:
-            file.unlink()
-        self.__music_remover.no_music_sound.parent.rmdir()
 
 
 def get_original_video(input_path: Path) -> Path | None:
@@ -84,9 +80,7 @@ def process_files() -> None:
     logging.info('Mass processing started')
 
     while original_video := get_original_video(input_path):
-        logging.info(f'Processing file "{original_video.relative_to(input_path)}"')
         RemoveMusicFromVideo(original_video, DemucsMusicRemover, input_path).process()
-        logging.info(f'"{original_video.relative_to(input_path)}": Processing finished')
     else:
         logging.info("There's no file to process")
 

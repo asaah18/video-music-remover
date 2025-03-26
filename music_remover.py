25545ba0d58c157b5b27f158ebd24dc3c3da95b5
abdurrahman <32853027+asaah18@users.noreply.github.com>
2025-03-26 17:36:12 +0300

    refactor `DemucsMusicRemover` to make it easily extendable to models of demucs
    Signed-off-by: abdurrahman <32853027+asaah18@users.noreply.github.com>

--- music_remover.py
+++ music_remover.py
@@ -33,18 +33,39 @@ class MusicRemover(ABC):
         pass
 
 
-class DemucsMusicRemover(MusicRemover):
+class DemucsMusicRemover(MusicRemover, ABC):
+
+    @abstractmethod
+    def _get_model(self) -> str:
+        """
+        the models to choose from are: htdemucs, mdx_extra_q
+        """
+        pass
+
     def _get_default_output_directory(self) -> Path:
         return Path('separated')
 
     def _get_no_music_audio_path(self, video_path: Path) -> Path:
-        return self._output_directory / f'htdemucs/{video_path.stem}/vocals.mp3'
+        return self._output_directory / f'{self._get_model()}/{video_path.stem}/vocals.mp3'
 
     def remove_music(self) -> None:
-        options: list[str] = ['--mp3', '--two-stems=vocals', '-o', self._output_directory.absolute()]
+        options: list[str] = ['--mp3',
+                              '--two-stems=vocals',
+                              '-n', self._get_model(),
+                              '-o', self._output_directory.absolute()]
         remove_music_command: list[str] = ['uv', 'run', 'demucs'] + options + [self._original_video.absolute()]
         subprocess.run(remove_music_command, encoding='utf-8', check=True)
         # raise exception if vocal sound is not created
         # exception raised manually because demucs command doesn't return error code
         if not self.no_music_sound.exists():
             raise subprocess.CalledProcessError(returncode=1, cmd=remove_music_command)
+
+
+class HTDemucsMusicRemover(DemucsMusicRemover):
+    """
+    the latest Hybrid Transformer Demucs model.
+    In some cases, this model can actually perform worse than previous models
+    """
+
+    def _get_model(self) -> str:
+        return "htdemucs"
